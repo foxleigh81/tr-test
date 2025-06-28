e46ec248711c2bd84377b6f8752d1e1c890527d9
Alex Foxleigh <alex@alexfoxleigh.com>
2025-06-28 18:34:02 +0100

    Minor refactor to move functions out of the component

--- src/components/data-display/medal/index.tsx
+++ src/components/data-display/medal/index.tsx
@@ -7,34 +7,33 @@ interface MedalProps {
   className?: string; // Optional class name for styling
 }
 
+const getColour = (medalRank: MedalRank): string => {
+  switch (medalRank) {
+    case 'gold':
+      return '#EFCD3C'; // Gold colour
+    case 'silver':
+      return '#929CA5'; // Silver colour
+    case 'bronze':
+      return '#744B26'; // Bronze colour
+  }
+};
+
+const getMedalLabel = (medalRank: MedalRank): string => {
+  switch (medalRank) {
+    case 'gold':
+      return 'Gold medal';
+    case 'silver':
+      return 'Silver medal';
+    case 'bronze':
+      return 'Bronze medal';
+  }
+};
+
 /**
  * Displays a medal as a coloured circle based on the medal type
  */
-export const Medal = memo<MedalProps>(({ rank, className = '' }) => {
-  const getColour = (medalRank: MedalRank): string => {
-    switch (medalRank) {
-      case 'gold':
-        return '#EFCD3C'; // Gold colour
-      case 'silver':
-        return '#929CA5'; // Silver colour
-      case 'bronze':
-        return '#744B26'; // Bronze colour
-    }
-  };
-
-  const getMedalLabel = (medalRank: MedalRank): string => {
-    switch (medalRank) {
-      case 'gold':
-        return 'Gold medal';
-      case 'silver':
-        return 'Silver medal';
-      case 'bronze':
-        return 'Bronze medal';
-    }
-  };
-
-  return (
-    <svg
+export const Medal = memo<MedalProps>(({ rank, className = '' }) =>  (
+  <svg
       width="20"
       height="20"
       viewBox="0 0 20 20"
@@ -49,8 +48,8 @@ export const Medal = memo<MedalProps>(({ rank, className = '' }) => {
         fill={getColour(rank)}
       />
     </svg>
-  );
-});
+  )
+);
 
 Medal.displayName = 'Medal';
 
